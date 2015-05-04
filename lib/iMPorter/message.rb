e2063dab3f210d88c0271a9e15cc4b599bde17c3
Ben Visser <theodore.r.visser@gmail.com>
2015-05-04 14:49:30 -0700

    PDF and TXT formatting both working

--- lib/iMPorter/message.rb
+++ lib/iMPorter/message.rb
@@ -1,13 +1,17 @@
 module IMPorter
   module Message
-    def self.write(user, message)
-      $pdf.font("/Library/Fonts/Times New Roman.ttf") do
-        $pdf.text "#{user}: #{message}"
+    def self.write(user, message, file_name, type)
+      if type.eql? 'PDF'
+        $pdf.font("/Library/Fonts/Times New Roman.ttf") do
+          $pdf.text "#{user}: #{message}"
+        end
+      else
+        File.open("#{file_name}.txt", 'a') {|f| f.write("#{user}: #{message}\n") }
       end
     end
 
     def self.read(id)
-      return $chat_db.execute('select is_from_me, text, cache_has_attachments, rowid from message where handle_id= ? ', id)
+      return $chat_db.execute("select is_from_me, text, cache_has_attachments, rowid, cache_roomnames from message where handle_id= ?", id)
     end
 
     def self.is_from_me?(message)
