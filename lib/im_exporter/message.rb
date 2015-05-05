8fe00269c2ee47c24291b33048184f3728835401
Ben Visser <theodore.r.visser@gmail.com>
2015-05-05 13:59:10 -0700

    added another method to message module to make code chunk smaller

--- lib/im_exporter/message.rb
+++ lib/im_exporter/message.rb
@@ -1,7 +1,7 @@
 module IM_Exporter
   module Message
-    def self.write(user, message, file_name, type)
-      if type.eql? 'PDF'
+    def self.write(user, message, file_name, file_type)
+      if file_type.eql? 'PDF'
         $pdf.font("/Library/Fonts/Times New Roman.ttf") do
           $pdf.text "#{user}: #{message}"
         end
@@ -12,6 +12,18 @@ module IM_Exporter
 
     def self.read(id)
       return $chat_db.execute("select is_from_me, text, cache_has_attachments, rowid, cache_roomnames from message where handle_id= ?", id)
+    end
+
+    def self.parse(message_row, contact_name, file_type)
+      if message.is_an_attachment?(message_row[2])
+        attachment.write(message_row[3]) if file_type.eql? 'PDF'
+      else
+        if message.is_from_me?(message_row[0])
+          message.write("me", message_row[1], contact_name, file_type)
+        else
+          message.write(contact_name, message_row[1], contact_name, file_type)
+        end
+      end if message_row[4].nil?
     end
 
     def self.is_from_me?(message)
